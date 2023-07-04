f38a1bb786c10734f0d7eb07ce6f1e8feb5bfbcc
Samara Perini <138031738+samaraperini@users.noreply.github.com>
2023-07-04 12:47:45 -0300

    Update exercicio_pratico.py

--- exercicio_pratico.py
+++ exercicio_pratico.py
@@ -1,3 +1,9 @@
+# Considere a seguinte agenda de disponibilidade de atendimento de uma clínica com vários médicos, cada um de uma especialidade distinta. 
+# Essa clínica possuí apenas três consultórios, e, por isso, no máximo três médicos podem atender por dia.
+# A clínica percebeu que os pacientes que desejam se consultar com mais de um médico sempre solicitam que o atendimento seja realizado no mesmo dia.
+# Você, como um(a) aspirante a desenvolvedor Python, aceitou o desafio de ajudar a automatizar essa tarefa. 
+# Para tanto, irá desenvolver duas funções que receberão a relação de disponibilidade de cada médico e que retornarão os dias disponíveis:
+
 cardiologista = {'terca', 'quarta'}
 ortopedista = {'terca', 'quinta'}
 dermatologista = {'segunda', 'quarta', 'sexta'}
